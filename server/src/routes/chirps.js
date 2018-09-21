11d9e2dbc280c87d15d202553a718ca4e5ccd700
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-20 23:27:43 -0500

    created sql pool insead of seperate connections per query

--- server/src/routes/chirps.js
+++ server/src/routes/chirps.js
@@ -4,8 +4,9 @@ import chirpsStore from '../chirpstore';
 import mysql from 'mysql';
 
 //establish connection to mysqldb
-let connection = mysql.createConnection(
+let connection = mysql.createPool(
     {
+        connectionLimit: 10,
         host: 'localhost',
         database: 'chirpr',
         user: 'chirprapp',
@@ -16,62 +17,54 @@ let connection = mysql.createConnection(
 let router = Router();
 
 //get router
-//if it has id, respond with one 
-//else send all to client
+//if it has id, respond with said chirp
+//else send all to client from database
 router.get('/:id?', (req, res) => {
-    connection.connect();
-    let id = req.params.id;
-    if (id) {
-        connection.query(`
-        select 
-            c.id,
-            u.name,
-            text
-        from chirps c
-        join users u on u.id=c.userid
-        where c.id=${id};`,
-         (err, results, fields) => {
-                if (err) {
-                    connection.end();
-                    return console.log(err);
-                }
-                connection.end()
-            });
-        // res.json(chirpsStore.GetChirp(id));
-    } else {
-        connection.query(`
-        select 
-            c.id,
-            u.name,
-            text
-        from chirps c
-        join users u on u.id=c.userid
-        ORDER BY c.id DESC;`,
-         (err, results, fields) => {
-                if (err) {
-                    connection.end();
-                    return console.log(err);
-                }
-                connection.end()
-                res.send(results);
-            });
-        // res.send(chirpsStore.GetChirps())
-    };
+        let id = req.params.id;
+        if (id) {
+            connection.query(`
+                select 
+                    c.id,
+                    u.name,
+                    text
+                from chirps c
+                join users u on u.id=c.userid
+                where c.id=${id};`,
+                (err, results, fields) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    res.send(results);
+                });
+            // res.json(chirpsStore.GetChirp(id));
+        } else {
+            connection.query(`
+                select 
+                    c.id,
+                    u.name,
+                    text
+                from chirps c
+                join users u on u.id=c.userid
+                ORDER BY c.id DESC;`,
+                (err, results, fields) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    res.send(results);
+                });
+        };
 });
 
 //post router
 //post resource sent from client to database
 //req.body is the data object {name: '',text: ''}
 router.post('/', (req, res) => {
-    connection.connect();
     let chirp = req.body.text;
     console.log(chirp);
     connection.query(`INSERT INTO CHIRPS(userid, text, location) VALUES(1, '${chirp}', 'Birmingham')`, (err, results, fields) => {
         if (err) {
-            connection.end();
             return console.log(err);
         }
-        connection.end()
     });
     res.sendStatus(200);
 });
@@ -81,13 +74,11 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
     let id = req.params.id;
     console.log(id);
-    // connection.query(`UPDATE CHIRPS SET TEXT = ${req.body.text} WHERE id=${id}`, (err, results, fields) => {
-    //     if (err) {
-    //         connection.end();
-    //         return console.log(err);
-    //     }
-    //     connection.end()
-    // });
+    connection.query(`UPDATE CHIRPS SET TEXT = ${req.body.text} WHERE id=${id}`, (err, results, fields) => {
+        if (err) {
+            return console.log(err);
+        }
+    });
     // chirpsStore.UpdateChirp(id, req.body);
     res.sendStatus(200);
 });
